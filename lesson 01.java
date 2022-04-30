590c3871e3ad21de689041ed6a9b5e3e0a5614be
Lubomiroleksandrovic <104529497+Lubomiroleksandrovic@users.noreply.github.com>
2022-04-30 22:36:03 +0300

    Add files via upload

---
+++ lesson 01.java
@@ -0,0 +1,133 @@
+package ua.lviv.lgs;
+
+public class homework01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*Завдання 1. Написати по одній змінній кожного типу примітивних даних  
+		byte number1 = 15;
+		short number2 = 127;
+		int number3 = 200;
+		long number4 = 225;
+		float number5 = 3.4f;
+		double number6 = 1.7;
+		char number7 = 655;
+		boolean number8 = true;  
+		
+		// Завдання 2. Вивести на консоль мінімальні та максимальні значення типів даних
+		System.out.println( Math.max (number1, number2 ));
+		System.out.println( Math.min (number1, number2 ));
+		System.out.println( Math.max (number3, number4 ));
+		System.out.println( Math.min (number3, number4 ));
+		System.out.println( Math.max (number5, number6 ));
+		System.out.println( Math.min (number5, number6 )); */
+		
+		/* Завдання 3. Створити масив, наповнити його 10 значеннями типу int, знайти мінімальні та максимальні значення, вивести на консоль
+		
+		int[] intArray = {10, 12, 0, 16, 18, 20, 22, 24, 26, 28};
+		int maxNum = intArray [0];
+		for (int j : intArray) {
+			if (j > maxNum)
+				maxNum = j;	}
+		
+			System.out.println("Maximum number = " + maxNum);*/
+			   
+			
+			
+         int[] list = {10, 12, 0, 16, 18, 20, 22, 24, 26, 28};
+			
+         int min = list [0];
+         for (int i =0; i < list.length; i++)
+         {
+	     if (list[i]< min)
+	      min = list [i];	
+          }	
+	      System.out.println("Min is" + min);	
+		
+		
+	
+		}
+	
+}
+	
+	
+	
+	
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+		
+		
+		
+		
+		
+
+		
+		
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+			
+			
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+
+
